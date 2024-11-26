661c64aa6d572915da4e0bfd343f2089c840749e
Sebastiaan-9 <sebastian.henrique@aluno.ifsertao-pe.edu.br>
2024-11-25 21:34:56 -0300

    atividade

--- pizzaria/app.py
+++ pizzaria/app.py
@@ -33,5 +33,39 @@ def cardapio():
     ]
     return render_template("cardapio.html", pizzas=pizzas)
 
+@app.route('/avaliacoes')
+def avaliacoes():
+    avaliacoes = [
+        {
+            "nome": "Charlie",
+            "pizza": "Calabresa",
+            "avaliacao": "3 estrelas",
+            "comentario": "Achei a pizza muito salgada!",
+            "data": "25/11/2024"
+        },
+        {
+            "nome": "Adam",
+            "pizza": "Mussarela",
+            "avaliacao": "4 estrelas",
+            "comentario": "Achei a pizza muito boa! Comprarei mais com certeza.",
+            "data": "25/11/2024"
+        }
+    ]
+    return render_template('avaliacoes.html', avaliacoes=avaliacoes)
+
+@app.route('/contatos')
+def contatos():
+    contatos = [
+        {
+            "email": "brenoprof1@gmai.co",
+            "telefone": "87 993094892"
+        }
+    ]
+    return render_template('contatos.html', contatos=contatos)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run()
